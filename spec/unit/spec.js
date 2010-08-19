1f3496f267426a4ffa08fbe25bd5d0200ee73229
Robert Lail <lailrc@a09-0016.cph.pri>
2010-08-19 13:57:41 -0500

    mix jspec's special grammar with normal JavaScript

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,10 +1,10 @@
 
-JSpec.describe('SetBuilder.Set', function() {
-  describe('data()', function() {
-    it('should return the data you passed it', function() {
+describe 'SetBuilder.Set'
+  describe '.data'
+    it 'should return the data you passed it'
       var expected_value = 'hi';
       var set = new SetBuilder.Set(expected_value);
       expect(set.data()).to(be, expected_value);
-    })
-  })
-})
+    end
+  end
+end
