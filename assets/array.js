40bd5bc4a6ccf1d4c1a921692eadd6eb2d4c7190
Robert Lail <lailrc@a09-0016.cph.pri>
2010-08-20 15:07:36 -0500

    made array.js more other-library friendly

--- assets/array.js
+++ assets/array.js
@@ -3,61 +3,72 @@
       ===========================================================
 */
 
-Object.keys = function(o) {
-  var keys = [];
-  for(key in o) {
-    keys.push(key);
-  }
-  return keys;  
-}
-
-
-
-
-
-Array.prototype.toSentence = function() {
-  switch(this.length) {
-    case 0:
-      return '';
-    case 1:
-      return this[0].toString();
-    case 2:
-      return this[0].toString() + ' and ' + this[1].toString();
-    default:
-      return this.slice(0, -1).join(', ') + ', and ' + this[this.length - 1].toString();
+if(!Object.keys) {
+  Object.keys = function(o) {
+    var keys = [];
+    for(key in o) {
+      keys.push(key);
+    }
+    return keys;  
   }
 }
 
-Array.prototype.dup = function(fn) {
-  return this.slice(0);
-}
 
-Array.prototype.each = function(fn) {
-  for(i=0; i<this.length; i++) {
-    fn(this[i]);
-  }
-}
 
-Array.prototype.collect = function(fn) {
-  var new_array = [];
-  for(var i=0; i<this.length; i++) {
-    new_array.push(fn(this[i]));
-  }
-  return new_array;
-}
-
-Array.prototype.inject = function(memo, fn) {
-  for(var i=0; i<this.length; i++) {
-    memo = fn(memo, this[i]);
-  }
-  return memo;
-}
-
-Array.prototype.find = function(fn) {
-  for(var i=0; i<this.length; i++) {
-    if(fn(this[i])) {
-      return this[i];
+if(!Array.prototype.toSentence) {
+  Array.prototype.toSentence = function() {
+    switch(this.length) {
+      case 0:
+        return '';
+      case 1:
+        return this[0].toString();
+      case 2:
+        return this[0].toString() + ' and ' + this[1].toString();
+      default:
+        return this.slice(0, -1).join(', ') + ', and ' + this[this.length - 1].toString();
     }
   }
-  return null;
+}
+if(!Array.prototype.dup) {
+  Array.prototype.dup = function(fn) {
+    return this.slice(0);
+  }
+}
+
+if(!Array.prototype.each) {
+  Array.prototype.each = function(fn) {
+    for(i=0; i<this.length; i++) {
+      fn(this[i]);
+    }
+  }
+}
+
+if(!Array.prototype.collect) {
+  Array.prototype.collect = function(fn) {
+    var new_array = [];
+    for(var i=0; i<this.length; i++) {
+      new_array.push(fn(this[i]));
+    }
+    return new_array;
+  }
+}
+
+if(!Array.prototype.inject) {
+  Array.prototype.inject = function(memo, fn) {
+    for(var i=0; i<this.length; i++) {
+      memo = fn(memo, this[i]);
+    }
+    return memo;
+  }
+}
+
+if(!Array.prototype.find) {
+  Array.prototype.find = function(fn) {
+    for(var i=0; i<this.length; i++) {
+      if(fn(this[i])) {
+        return this[i];
+      }
+    }
+    return null;
+  }
 }
