89135f24eb404834257340e17ec92d9fb6b2eec1
Robert Lail <lailrc@a09-0016.cph.pri>
2010-08-26 11:15:48 -0500

    how to make a symbol out of a period

--- lib/set_builder/modifiers/date_modifier.rb
+++ lib/set_builder/modifiers/date_modifier.rb
@@ -9,7 +9,7 @@ module SetBuilder
       
       def self.operators
         {
-          '.' => [],
+          :"." => [],
           :before => [:date],
           :after => [:date],
           :on => [:date],
@@ -21,6 +21,8 @@ module SetBuilder
       
       def build_conditions_for(selector)
         case operator
+        when :".":
+          ["#{selector} IS NOT NULL"]
         when :before
           ["#{selector}<?", format_value]
         when :after
@@ -28,35 +30,33 @@ module SetBuilder
         when :on
           ["#{selector}=?", format_value]
         when :in_the_last
-          ["#{selector_for_period(selector)}<=?", format_value]
+          ["#{selector}>=?", format_value]
+        else
+          []
         end
-        []
       end
       
       
       
-    private
+    protected
       
       
       
       def format_value
-        values[0]
-      end
-      
-      
-      
-      # !todo: this might not result in *exactly* expected values
-      def selector_for_period(selector)
-        # datediff = "DATEDIFF(CURDATE(), #{selector})"
-        case values[1]
-        when "years", "year"
-          "DATEDIFF(CURDATE(), #{selector})/365"
-        when "months", "month"
-          "DATEDIFF(CURDATE(), #{selector})/30"
-        when "weeks", "week"
-          "(DATEDIFF(CURDATE(), #{selector})/7)"
-        when "days", "day"
-          "DATEDIFF(CURDATE(), #{selector})"
+        case operator
+        when :in_the_last
+          case values[1]
+          when "years", "year"
+            values[0].to_i.years.ago
+          when "months", "month"
+            values[0].to_i.months.ago
+          when "weeks", "week"
+            values[0].to_i.weeks.ago
+          when "days", "day"
+            values[0].to_i.days.ago
+          end          
+        else
+          values[0].to_date
         end
       end
       
