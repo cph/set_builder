34a0de07e76d6756d31d808d21b5694d4770aacc
Ben Govero and Bob Lail <pair=ben.govero+bob.lail@cphepdev.com>
2015-03-06 16:53:28 -0600

    Finished refactoring javascript to use to DSL

--- spec/unit/set_builder.spec.js
+++ spec/unit/set_builder.spec.js
@@ -73,23 +73,19 @@ describe 'SetBuilder'
   describe '.Trait'
   
     describe '.constructor'
-      it 'should correctly parse simple parameters'
+      it 'should correctly parse the name of the trait'
         var trait = new SetBuilder.Trait([['string', 'who are'], ['name', 'awesome']]);
         expect(trait.name()).to(be, 'awesome');
-        expect(trait.toString()).to(eql, 'who are awesome');
       end
     
-      it 'should correctly identify direct objects'
+      it 'should correctly identify when it expects a direct objects'
         var trait = new SetBuilder.Trait([['string', 'who have'], ['name', 'attended'], ['direct_object_type', 'string']]);
-        expect(trait.name()).to(be, 'attended');
-        expect(trait.requires_direct_object()).to(be, true);
-        expect(trait.toString()).to(eql, 'who have attended');
+        expect(trait.requiresDirectObject()).to(be, true);
       end
     
       it 'should correctly parse paramters with modifiers'
         var trait = new SetBuilder.Trait([['string', 'who were'], ['name', 'born'], ['modifier', 'date']]);
         expect(trait.modifiers().length).to(be, 1);
-        expect(trait.toString()).to(eql, 'who were born');
       end
     end
     
@@ -110,11 +106,11 @@ describe 'SetBuilder'
   
     describe '.find'
       it 'should get a SetBuilder.Trait object by name'
-        expect(traits.__find('awesome').toString()).to(eql, 'who are awesome');
-        expect(traits.__find('died').toString()).to(eql,    'who died');
-        expect(traits.__find('born').toString()).to(eql,    'who were born');
-        expect(traits.__find('attended').toString()).to(eql,'who have attended');
-        expect(traits.__find('name').toString()).to(eql,    'whose name');
+        expect(traits.__find('awesome').name()).to(eql, 'awesome');
+        expect(traits.__find('died').name()).to(eql,    'died');
+        expect(traits.__find('born').name()).to(eql,    'born');
+        expect(traits.__find('attended').name()).to(eql,'attended');
+        expect(traits.__find('name').name()).to(eql,    'name');
       end
     end
     
