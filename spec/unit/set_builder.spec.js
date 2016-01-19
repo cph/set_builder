5315fa32336e09d4bd3bc74e95156145d53257fe
Bob Lail <bob.lailfamily@gmail.com>
2016-01-19 09:44:10 -0600

    [skip] Normalized whitespace (1m)

--- spec/unit/set_builder.spec.js
+++ spec/unit/set_builder.spec.js
@@ -38,9 +38,9 @@ describe 'SetBuilder'
         is: ['string']
       }
     });
-    
+
     SetBuilder.registerValueMap('school', [['1','Concordia'], ['2','McKendree']]);
-    
+
     set_data = [
       { trait: 'awesome', enums: ['are'] },
       { trait: 'attended', enums: ['have'], school: 2 },
@@ -48,7 +48,7 @@ describe 'SetBuilder'
       { trait: 'name', modifiers: [
         { operator: 'is', values: ['Jerome'] }] }
     ];
-    
+
     set_data_hash = {
       '0': { trait: 'awesome', enums: {'0': 'are'} },
       '1': { trait: 'attended', enums: {'0': 'have'}, school: 2 },
@@ -57,67 +57,67 @@ describe 'SetBuilder'
         '0': { operator: 'is', values: {'0': 'Jerome'} } } }
     };
   end
-  
-  
-  
-  
+
+
+
+
   describe '.getValueMap'
     it 'should return an array of arrays'
       expect(SetBuilder.getValueMap('school')).to(eql, [['1','Concordia'], ['2','McKendree']]);
     end
   end
-  
+
   describe '.getValue'
     it 'should return a value based on the key and name'
       expect(SetBuilder.getValue('school', 1)).to(be, 'Concordia')
       expect(SetBuilder.getValue('school', '1')).to(be, 'Concordia')
     end
-    
+
     it 'should return what it was passed if there is no value map for the key'
       expect(SetBuilder.getValue('band', 1)).to(be, 1)
     end
   end
-  
+
   describe '.getValueMaps'
     it 'should return the names of the value maps registered'
       expect(SetBuilder.getValueMaps()).to(eql, ['school']);
     end
   end
-  
-  
-  
+
+
+
   describe '.Trait'
     describe '.constructor'
       it 'should correctly parse the name of the trait'
         var trait = new SetBuilder.Trait([['string', 'who are'], ['name', 'awesome']]);
         expect(trait.name()).to(be, 'awesome');
       end
-    
+
       it 'should correctly identify when it expects a direct objects'
         var trait = new SetBuilder.Trait([['string', 'who have'], ['name', 'attended'], ['direct_object_type', 'string']]);
         expect(trait.requiresDirectObject()).to(be, true);
       end
-    
+
       it 'should correctly parse paramters with modifiers'
         var trait = new SetBuilder.Trait([['string', 'who were'], ['name', 'born'], ['modifier', 'date']]);
         expect(trait.modifiers().length).to(be, 1);
       end
     end
   end
-  
-  
-  
+
+
+
   describe '.Traits'
     before_each
       traits = SetBuilder.traits();
     end
-  
+
     describe '.length'
       it 'should have parsed the data structure correctly'
         expect(traits.length()).to(be, 6);
       end
     end
-  
+
     describe '.find'
       it 'should get a SetBuilder.Trait object by name'
         expect(traits.__find('awesome').name()).to(eql, 'awesome');
@@ -127,7 +127,7 @@ describe 'SetBuilder'
         expect(traits.__find('name').name()).to(eql,    'name');
       end
     end
-    
+
   end
 
 
@@ -159,12 +159,12 @@ describe 'SetBuilder'
         var set = new SetBuilder.Set(set_data);
         expect(set.constraints().length).to(be, 4);
       end
-        
+
       it 'should parse hash-style objects as well as arrays'
         var set = new SetBuilder.Set(set_data_hash);
         expect(set.constraints().length).to(be, 4);
       end
-        
+
       it 'should consider two sets with identical constraints equal'
         var set1 = new SetBuilder.Set(set_data);
         var set2 = new SetBuilder.Set(set_data_hash);
@@ -172,13 +172,13 @@ describe 'SetBuilder'
         expect(set2.isEqualTo(set1)).to(be, true);
       end
     end
-  
+
     describe '.toString'
       it 'should generate the natural language description of a simple set'
         var simple_set = new SetBuilder.Set([{ trait: 'awesome', enums: {'0': 'are'} }]);
         expect(simple_set.toString()).to(eql, 'who are awesome');
       end
-      
+
       it 'should generate the natural language description of a complex set'
         var set = new SetBuilder.Set(set_data);
         var expected_string = 'who are awesome, who have attended McKendree, who have died, and whose name is Jerome'
@@ -196,6 +196,6 @@ describe 'SetBuilder'
         expect(set.toString()).to(eql, expected_string);
       end
     end
-    
+
   end
 end
